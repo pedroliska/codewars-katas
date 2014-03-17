973be2dc0f2ef9a6d648aca70873281572f702ec
Pedro Liska <pedro.liska@extendhealth.com>
2014-03-17 09:38:34 -0600

    using map instead of times

--- ruby/fizz-buzz.rb
+++ ruby/fizz-buzz.rb
@@ -1,12 +1,9 @@
 def fizzbuzz(n)
-  ret_val = []
-  n.times do |i| 
-    num = i + 1
-    word = 'Fizz' if num % 3 == 0
-    word = "#{word}Buzz" if num % 5 == 0
-    ret_val.push(word || num)
+  (1..n).map do |i|
+    word = 'Fizz' if i % 3 == 0
+    word = "#{word}Buzz" if i % 5 == 0
+    word || i
   end
-  ret_val
 end
 
 puts fizzbuzz 16
