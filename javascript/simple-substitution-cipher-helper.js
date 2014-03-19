fdc1b014e5eff1cd513a9d9d883bdf291434d96b
Pedro Liska <pedro.liska@extendhealth.com>
2014-03-19 09:33:40 -0600

    really liked this one

---
+++ javascript/simple-substitution-cipher-helper.js
@@ -0,0 +1,30 @@
+function SubstitutionCipher(abc1, abc2) {
+
+  var self = this;
+
+  function getTranslator(abcFrom, abcTo) {
+
+    var hash = {};
+    abcFrom
+      .split('')
+      .forEach(function (fromItem, index) { 
+          hash[fromItem] = abcTo[index]; 
+      });
+
+    return function(str) { 
+      return str
+        .split('')
+        .map(function(chr) {
+          if (hash.hasOwnProperty(chr))
+            return hash[chr];
+          else 
+            return chr;
+        })
+        .join('');
+    };
+
+  }
+
+  this.encode = getTranslator(abc1, abc2);
+  this.decode = getTranslator(abc2, abc1);
+}
