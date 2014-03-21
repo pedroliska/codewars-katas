260c249f26321a8dc9fabfe06831f4e6b90a6510
Pedro Liska <pedro.liska@extendhealth.com>
2014-03-21 10:40:47 -0600

    completed the vector class kata

--- javascript/vector-class.js
+++ javascript/vector-class.js
@@ -8,10 +8,38 @@ var Vector = function (components) {
   this.getComponents = function() {
     return self.components;
   };
-  
+
+  function parallelOperation(vect1, vect2, func) {
+    if (vect1.getLength() !== vect2.getLength())
+      throw "The vectors need to be of the same length";
+    var newCompoments = vect1.getComponents().map(function (item, index) {
+      return func(item, vect2.getComponents()[index]);
+    });
+    return new Vector(newCompoments);
+  }
+
   this.add = function (vect) {
-    if (vect.getLength() !== self.getLength())
-      throw "You cannot add vectors of different lengths";
+    return parallelOperation(self, vect, function (a,b) { return a+b; });
+  };
+  this.subtract = function (vect) {
+    return parallelOperation(self, vect, function (a,b) { return a-b; });
+  };
+  this.dot = function (vect) {
+    var paralledFunc = function (a,b) { return a*b; };
+    var reducerFunc = function (prev, curr, index) {
+      return prev+curr;
+    };
+    var product = parallelOperation(self, vect, paralledFunc);
+    return product.getComponents().reduce(reducerFunc);
+  };
+  this.norm = function () {
+    var newCompoments = self.getComponents()
+      .map(function (item) {
+        return item*item;
+      }).reduce(function (prev, curr) {
+        return prev + curr;
+      });
+    return Math.sqrt(newCompoments);
   };
   this.equals = function (vect) {
     if (vect.getLength() !== self.getLength())
@@ -32,12 +60,3 @@ var Vector = function (components) {
 };
 
 
-
-var a = new Vector([1,2]);
-var b = new Vector([3,4]);
-var c = new Vector([5,6,7,8]);    
-
-Test.assertEquals(a.toString(), '(1,2)');
-Test.assertEquals(a.getLength(), 2);
-Test.expect((new Vector[1,2, 3]).equals(new Vector[1,2]));
-//Test.expect(a.add(b).equals(new Vector([4,6])));
