49bcc3df6e870d2e9bc979e3d9a8fa633b5e7520
Pedro Liska <pedro.liska@extendhealth.com>
2014-03-26 10:00:38 -0600

    created some jasmine tests for the vector class kata

---
+++ javascript/vector-class/tests.js
@@ -0,0 +1,14 @@
+﻿describe("Vector Class Tests", function () {
+
+  var vect1 = new Vector([1, 2, 3]);
+  var vect2 = new Vector([2, 2, 2]);
+
+  it('should have a toString method', function() {
+    expect(vect1.toString()).toEqual('(1,2,3)');
+  });
+
+  it('should have an add method', function() {
+    expect(vect1.add(vect2).toString()).toEqual('(3,4,5)');
+  });
+
+});
