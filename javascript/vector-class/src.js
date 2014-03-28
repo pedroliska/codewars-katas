bc83a4f8f45d7c24a968e66ac3c00f2b32d6ca2b
Pedro Liska <pedro.liska@extendhealth.com>
2014-03-28 09:02:53 -0600

    first attempt after the jasmine tests, was able to complete it quickly with the tests as a guide

--- javascript/vector-class/src.js
+++ javascript/vector-class/src.js
@@ -1,79 +1,46 @@
 var Vector = function (components) {
 
-    var self = this;
-    this.components = components;
+  var self = this;
 
-    function parallelOperation(vect1, vect2, operation) {
+  this.toString = function() {
+    return '(' + components.join(',') + ')';
+  };
+  this.equals = function(vect) {
+    return self.toString() == vect.toString();
+  }
+  this.getComponents = function() {
+    return components;
+  };
 
-        var comp1 = vect1.getComponents(),
-            comp2 = vect2.getComponents();
+  function parallelOperation(vect1, vect2, operation) {
+    var comp1 = vect1.getComponents(),
+        comp2 = vect2.getComponents();
 
-        if (comp1.length != comp2.length)
-            throw new Error('Vectors need to be of the same length');
+    if (comp1.length !== comp2.length)
+      throw new Error('Both vectors need to be of the same length');
 
-        var newCompoments = comp1.map(function (item, index) {
-            return operation(item, comp2[index]);
-        });
-        return new Vector(newCompoments);
-    }
-    this.getComponents = function () {
-        return self.components;
-    };
-    this.add = function (vect) {
-        return parallelOperation(self, vect, function (a, b) { return a + b; });
-    };
-    this.subtract = function (vect) {
-        return parallelOperation(self, vect, function (a, b) { return a - b; });
-    };
-    this.dot = function (vect) {
+    var newComp = comp1.map(function(item, index) {
+      return operation(item, comp2[index]);
+    });
+    return new Vector(newComp);
+  }
+  this.add = function(vect) {
+    return parallelOperation(self, vect, function(a, b) { return a + b; });
+  };
+  this.subtract = function(vect) {
+    return parallelOperation(self, vect, function(a, b) { return a - b; });
+  };
 
-        var product,
-            comp1 = self.getComponents(),
-            comp2 = vect.getComponents();
-        parallelOp = function (a, b) { return a * b; },
-        reduceOp = function (a, b) { return a + b; };
-
-        if (comp1.length != comp2.length)
-            throw 'Vectors need to be of the same length';
-
-        product = parallelOperation(self, vect, parallelOp);
-
-        return product.getComponents().reduce(function (accum, curr) {
-            return reduceOp(accum, curr);
-        });
-    };
-    this.norm = function () {
-        var temp = self.getComponents()
-          .map(function (item) { return item * item; })
-          .reduce(function (accum, curr) { return accum + curr; });
-        return Math.sqrt(temp);
-    };
-    this.equals = function(vect) {
-        var comp1 = self.getComponents(),
-            comp2 = vect.getComponents();
-        if (comp1.length != comp2.length)
-            return false;
-        return comp1.reduce(function(aggregate, curr, index) {
-            if (!aggregate) return false;
-            return curr == comp2[index];
-        }, true);
-
-    };
-    this.toString = function() {
-        return '(' + self.getComponents().join(',') + ')';
-    };
+  this.dot = function(vect) {
+    var parallelResult = parallelOperation(self, vect, function (a, b) { return a * b; });
+    return parallelResult.getComponents().reduce(function(accum, item) {
+      return accum + item;
+    });
+  };
+  this.norm = function() {
+    var squaredAndAdded = self.getComponents()
+      .map(function (item) { return item * item; })
+      .reduce(function(accum, item) { return accum + item; });
+    return Math.sqrt(squaredAndAdded);
+  };
 }
-
-// ---------------------------------
-
-var vect1 = new Vector([1, 2]);
-var vect2 = new Vector([2, 2]);
-
-console.log(vect1.toString());
-
-console.log(vect1.equals(vect2));
-console.log(vect1.equals(vect1));
-console.log(vect1.equals(new Vector(2,3,4)));
-
-console.log(vect1.add(vect2).toString());
-console.log(vect1.dot(vect2).toString());
