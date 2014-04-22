57b623b8c38c0bab92e33441cd7ee848d1102dae
Pedro Liska <pedro.liska@extendhealth.com>
2014-04-22 08:34:29 -0600

    final attempt at the human friendly random numbers kata

--- javascript/human-friendly-random-numbers/src.js
+++ javascript/human-friendly-random-numbers/src.js
@@ -1,26 +1,23 @@
-function RNG(upperLimit) {
-  this.upperLimit = upperLimit;
+function RNG(upper) {
+  this.upper = upper;
   this.availableNumbers = [];
 }
-RNG.prototype.rand = function () {
-
-  var randomIndex, retVal;
-
+RNG.prototype.rand = function() {
   if (!this.availableNumbers.length)
-    this.availableNumbers = initArray(this.upperLimit);
+    this.availableNumbers = initArray(this.upper);
 
-  randomIndex = randomInt(this.availableNumbers.length);
-  retVal = this.availableNumbers[randomIndex];
+  var randomIndex = randomInt(this.availableNumbers.length);
+  var retVal = this.availableNumbers[randomIndex];
   this.availableNumbers.splice(randomIndex, 1);
   return retVal;
 }
 
-function randomInt(upper) {
-  return Math.floor(Math.random() * upper);
-}
 function initArray(upper) {
   var i, retVal = [];
   for (i = 0; i < upper; i++)
     retVal.push(i);
   return retVal;
+}
+function randomInt(upper) {
+  return Math.floor(Math.random() * upper);
 }
