8b539409357e849cabbfe4e129e57c09d3234726
Pedro Liska <pedro.liska@extendhealth.com>
2014-04-30 08:36:46 -0600

    completed the valid christmas tree kata

--- javascript/valid-binary-christmas-tree/src.js
+++ javascript/valid-binary-christmas-tree/src.js
@@ -1,5 +1,4 @@
 // http://www.codewars.com/kata/52aebd2423b44356b8000578
-//Test Failed: Test failed for following tree: {"ornament":"star","color":"yellow","left":{"ornament":"candy cane","color":"blue"},"right":{"ornament":"stocking","color":"blue"}} - Expected: true, instead got: false
 
 function isValidTree(root) {
 
@@ -18,9 +17,9 @@ function traverse(node) {
   if (!node)
     return true;
 
-  if (!node.left && !node.right && node.ornament  !== 'blue')
+  if (!node.left && !node.right && node.color  !== 'blue')
     return false;
-  if ((node.left || node.right) && node.ornament !== 'red')
+  if ((node.left || node.right) && node.color !== 'red')
     return false;
 
   if (!traverse(node.left))
---
+++ javascript/valid-binary-christmas-tree/tests.js
@@ -0,0 +1,19 @@
+﻿describe("Valid binary christmas tree tests", function () {
+
+  it('should deal with valid tree 2 children nodes', function() {
+    
+    var tree = {
+      "ornament":"star",
+      "color":"yellow",
+      "left": {
+        "ornament":"candy cane",
+        "color":"blue"},
+      "right": {
+        "ornament":"stocking",
+        "color":"blue"}};
+
+     expect(isValidTree(tree)).toBe(true);
+
+  });
+
+});
