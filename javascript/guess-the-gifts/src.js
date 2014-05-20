c9acf18089591490b41d92810cd9332cd3529860
Pedro Liska <pedro.liska@extendhealth.com>
2014-05-20 08:16:47 -0600

    finished the guess-the-gifts kata, need to use the "some" array method in the next attempt

--- javascript/guess-the-gifts/src.js
+++ javascript/guess-the-gifts/src.js
@@ -1,13 +1,27 @@
+// http://www.codewars.com/kata/52ae6b6623b443d9090002c8
+
 function guessGifts(wishlist, presents) {
 
   return wishlist.filter(function(listItem) {
     // for each wishlist item, see if it matches a present
     return presents.find(function(present) {
       return (listItem.size == present.size)
-        && (listitem.clatters == present.clatters)
-        && (listitem.weight == present.weight);
+        && (listItem.clatters == present.clatters)
+        && (listItem.weight == present.weight);
     });
 
+  }).map(function(listItem) {
+    return listItem.name;
   });
 
+}
+
+Array.prototype.find = function(checkFn) {
+  for (var i = 0, value; i < this.length; i++) {
+    value = this[i];
+
+    if (checkFn(value))
+      return value;
+  }
+  return undefined;
 }
