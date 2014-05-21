2abf531c87635b80ca95c88a91c6c470edeee65f
Pedro Liska <pedro.liska@extendhealth.com>
2014-05-21 09:03:30 -0600

    second attempt at the "guess the gifts" kata, next time try to use an array of property names for the comparison, is it too ugly?

--- javascript/guess-the-gifts/src.js
+++ javascript/guess-the-gifts/src.js
@@ -1,27 +1,13 @@
 // http://www.codewars.com/kata/52ae6b6623b443d9090002c8
 
-function guessGifts(wishlist, presents) {
-
-  return wishlist.filter(function(listItem) {
-    // for each wishlist item, see if it matches a present
-    return presents.find(function(present) {
-      return (listItem.size == present.size)
-        && (listItem.clatters == present.clatters)
-        && (listItem.weight == present.weight);
+function guessGifts(wishList, presents) {
+  return wishList.filter(function(wishItem) {
+    return presents.some(function(present) {
+      return present.size == wishItem.size
+        && present.clatters == wishItem.clatters
+        && present.weight == wishItem.weight;
     });
-
-  }).map(function(listItem) {
-    return listItem.name;
+  }).map(function(wishItem) {
+    return wishItem.name;
   });
-
-}
-
-Array.prototype.find = function(checkFn) {
-  for (var i = 0, value; i < this.length; i++) {
-    value = this[i];
-
-    if (checkFn(value))
-      return value;
-  }
-  return undefined;
 }
