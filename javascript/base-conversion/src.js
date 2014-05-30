3df05342e8295d148db624f19dd0ce98d714fabe
Pedro Liska <pedro.liska@extendhealth.com>
2014-05-30 10:04:01 -0600

    base-conversion kata: converted the input number to decimal base, still not done

--- javascript/base-conversion/src.js
+++ javascript/base-conversion/src.js
@@ -1,7 +1,23 @@
-function convert(input, source, target) {
+function convert(input, sourceAlph, targetAlph) {
+
+  var sourcePositionMap = getPositionMap(sourceAlph);
+  var targetPositionMap = getPositionMap(targetAlph);
+
   // convert decimal base
 
+  var inputInDecimal = input.split('').reverse().reduce(function(accum, item, index) {
+    return accum 
+      + (Math.pow(sourceAlph.length, index) * sourcePositionMap[item]);
+  }, 0);
+
+  return inputInDecimal;
   // convert to target base
 
   return "asdf";
+}
+function getPositionMap(alph) {
+  return alph.split('').reduce(function (accum, item, index) {
+    accum[item] = index;
+    return accum;
+  }, {});
 }
