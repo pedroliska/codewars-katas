f44fb10e0857ab1d4031a7ec7bec9a9a078f06b9
Pedro Liska <pedro.liska@extendhealth.com>
2014-06-02 09:55:14 -0600

    base-conversion: some conversions work but still getting some errors

--- javascript/base-conversion/src.js
+++ javascript/base-conversion/src.js
@@ -1,20 +1,30 @@
 function convert(input, sourceAlph, targetAlph) {
 
-  var sourcePositionMap = getPositionMap(sourceAlph);
-  var targetPositionMap = getPositionMap(targetAlph);
+  var
+    sourcePositionMap = getPositionMap(sourceAlph),
+    targetPositionMap = getPositionMap(targetAlph),
+    sourceBase = sourceAlph.length,
+    targetBase = targetAlph.length,
+    dividend,
+    remainderPositions = [];
 
   // convert decimal base
-
   var inputInDecimal = input.split('').reverse().reduce(function(accum, item, index) {
     return accum 
-      + (Math.pow(sourceAlph.length, index) * sourcePositionMap[item]);
+      + (Math.pow(sourceBase, index) * sourcePositionMap[item]);
   }, 0);
 
-  return inputInDecimal;
   // convert to target base
-
-  return "asdf";
+  dividend = inputInDecimal;
+  do {
+    remainderPositions.push(dividend % targetBase);
+    dividend = Math.floor(dividend / targetBase);
+  } while (dividend > 0);
+  return remainderPositions.map(function(position) {
+    return targetPositionMap[position];
+  }).reverse().join('');
 }
+
 function getPositionMap(alph) {
   return alph.split('').reduce(function (accum, item, index) {
     accum[item] = index;
